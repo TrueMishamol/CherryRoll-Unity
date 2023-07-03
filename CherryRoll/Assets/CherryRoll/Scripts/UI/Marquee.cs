4d47505be17761ce6d59e1ae4d20b938c04e55dd
Mishamol <101403719+TrueMishamol@users.noreply.github.com>
2023-07-03 21:43:35 +0300

    Main Menu UI update - step 2
    Add & setup Marquee

---
+++ CherryRoll/Assets/CherryRoll/Scripts/UI/Marquee.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marquee : MonoBehaviour {
+
+    [SerializeField] private float speed = 4f;
+    private Vector3 startPosition;
+    private Vector3 currentPosition;
+    private Vector3 directionVector;
+
+    private float height;
+    private float width;
+
+    private enum Direction {
+        up,
+        down,
+        left,
+        right,
+    }
+
+    [SerializeField] private Direction direction;
+
+    private void Awake() {
+        startPosition = transform.localPosition;
+
+        height = GetComponent<RectTransform>().rect.height;
+        width = GetComponent<RectTransform>().rect.width;
+    }
+
+    private void Update() {
+        switch (direction) {
+            case Direction.up:
+                directionVector = Vector3.up;
+                break;
+            case Direction.down:
+                directionVector = Vector3.down;
+                break;
+            case Direction.left:
+                directionVector = Vector3.left;
+                break;
+            case Direction.right:
+                directionVector = Vector3.right;
+                break;
+        }
+
+        transform.Translate(directionVector * speed * Time.unscaledTime);
+
+        currentPosition = transform.localPosition;
+
+        if (Mathf.Abs(currentPosition.y - startPosition.y) > height) {
+            transform.localPosition = startPosition;
+        }
+        if (Mathf.Abs(currentPosition.x - startPosition.x) > width) {
+            transform.localPosition = startPosition;
+        }
+    }
+}
